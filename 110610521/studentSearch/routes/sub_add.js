38ebfd4699865bad93a04a93b051b6a1b4539a72
oxolll <s110610521@student.nqu.edu.tw>
2019-12-24 22:30:09 +0800

    ChLiZo

--- 110610521/studentSearch/routes/sub_add.js
+++ 110610521/studentSearch/routes/sub_add.js
@@ -3,33 +3,42 @@ var router = express.Router();
 var pool = require('./lib/db.js');
 
 router.post('/', function (req, res, next) {
-
-
-
-    var ll = req.body.name;
+    var ll = [];
+    /*console.log(req.body.name);*/
+    if(req.body.name==undefined){
+        res.redirect('/add');
+    }
+    
+    if (typeof (req.body.name) == 'string') {
+        ll[0] = req.body.name;
+        n = 1;
+    } else {
+        n = req.body.name.length;
+        for (var i = 0; i < req.body.name.length; i++) {
+            ll[i] = req.body.name[i];
+        }
+    }
     //ll紀錄req長度
-    for (var i = 0; i < ll.length; i++) {
+    for (var i = 0; i < n; i++) {
         pool.query("select c_id from course where c_name=?", [ll[i]], function (err, result) {  //根據帳號讀取資料
             if (err) throw err;
             pool.query('select * from choose where id = ? AND c_id = ?', [req.session.username, result[0].c_id], function (err, search) {
                 if (err) throw err;
                 if (!search.length) {
-                    pool.query('insert into choose(id,c_id) values(?,?)', [req.session.username, result[0].c_id], function (err, results) {  //根據帳號讀取資料
+                    pool.query('insert into choose(id,c_id) values(?,?)', [req.session.username, result[0].c_id], function (err, asd) {  //根據帳號讀取資料
                         if (err) throw err;
                     })
-                }
-                else {
+                } else {
                     return;
                 }
             })
 
         })
     }
-    res.render('sub_add', {
+    res.redirect('/test1');
+    /*res.render('sub_add', {
         name: req.body.name
-        //,
-        //c_id : req.body.c_id
-    });
+    });*/
 });
 
 module.exports = router;
--- 110610521/studentSearch/routes/sub_drop.js
+++ 110610521/studentSearch/routes/sub_drop.js
@@ -4,9 +4,22 @@ var pool = require('./lib/db.js');
 
 router.post('/', function (req, res, next) {
 
-    var ll = req.body.name;
+    if(req.body.name==undefined){
+        res.redirect('/drop');
+    }
 
-    for (var i = 0; i < ll.length; i++) {
+    var ll = [];
+    if (typeof (req.body.name) == 'string') {
+        ll[0] = req.body.name;
+        n = 1;
+    } else {
+        n = req.body.name.length;
+        for (var i = 0; i < req.body.name.length; i++) {
+            ll[i] = req.body.name[i];
+        }
+    }
+
+    for (var i = 0; i < n; i++) {
         pool.query("select c_id from course where c_name=?", [ll[i]], function (err, result) {  //根據帳號讀取資料
             if (err) throw err;
             pool.query('delete from choose where id=? AND c_id=?', [req.session.username, result[0].c_id], function (err, search) {
@@ -14,13 +27,15 @@ router.post('/', function (req, res, next) {
             })
         })
     }
-    pool.query('select * from course INNER JOIN choose ON course.c_id=choose.c_id WHERE choose.id=?',[req.session.username],function(err,result2){
+
+    res.redirect('/test1');
+    /*pool.query('select * from course INNER JOIN choose ON course.c_id=choose.c_id WHERE choose.id=?',[req.session.username],function(err,result2){
         if(err) throw err;
         console.log(result2);
         res.render('sub_drop', {
             name : result2
         });       
-    })
+    })*/
 });
 
 module.exports = router;
